e8983c79ea9cf623e564ae70df42f25ac7796f47
euichan2 <euichan1108@gmail.com>
2022-07-11 22:56:06 +0900

    User Model

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.core.validators import RegexValidator
+from .managers import UserManager
+from django.utils.translation import gettext_lazy as _
 
 
 class User(AbstractUser):
@@ -13,9 +15,13 @@ class User(AbstractUser):
         blank=True,
         validators=[RegexValidator(r"^010-?[1-9]\d{3}-?\d{4}$")],
     )
-
     gender = models.CharField(max_length=1, blank=True, choices=GenderChoices.choices)
+    username = None
+    email = models.EmailField(_('email address'), unique=True)
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
 
-    @property
-    def name(self):
-        return f"{self.first_name} {self.last_name}"
+    objects = UserManager()
+
+    def __str__(self):
+        return self.email
