2fc830737fb922907cc6188dd42a3f2bdb6c86f1
euichan2 <euichan1108@gmail.com>
2022-07-12 00:04:33 +0900

    login

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 
 urlpatterns = [
-    path('v1/registration/', include('dj_rest_auth.registration.urls')),
+    path('', include('dj_rest_auth.urls')),
+    path('registration/', include('dj_rest_auth.registration.urls')),
 ]
