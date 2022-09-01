4a5e93e7d94ab56eac8910c87fcf7188aff3fca3
PRESSO <caff1nepill@gmail.com>
2022-09-02 03:43:23 +0900

    change settings for deploy

--- teenlief/settings.py
+++ teenlief/settings.py
@@ -15,31 +15,30 @@ import os
 import sys
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-import dj_database_url
 from django.core.exceptions import ImproperlyConfigured
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
+# TODO: 배포할 땐 항상 False로 변경
+DEBUG = True
 
-# SECURITY WARNING: keep the secret key used in production secret!
-secret_file = os.path.join(BASE_DIR, 'secrets.json')  # secrets.json 파일 위치를 명시
-
+secret_file = os.path.join(BASE_DIR, 'secrets.json')
 with open(secret_file) as f:
     secrets = json.loads(f.read())
 
 
-def get_secret(setting):
+def get_env_variable(var_name):
     try:
-        return secrets[setting]
+        if DEBUG:
+            return secrets[var_name]
+        else:
+            return os.environ[var_name]
     except KeyError:
-        error_msg = "Set the {} environment variable".format(setting)
-        raise ImproperlyConfigured(error_msg)
+        error_msg = "Set the {} environment variable".format(var_name)
+    raise ImproperlyConfigured(error_msg)
 
 
-SECRET_KEY = get_secret("SECRET_KEY")
+SECRET_KEY = get_env_variable("SECRET_KEY")
 
 for key, value in secrets.items():
     setattr(sys.modules[__name__], key, value)
@@ -109,7 +108,19 @@ WSGI_APPLICATION = 'teenlief.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = get_secret("DATABASES")['test']
+DATABASES = {
+    'default': {
+        'ENGINE': get_env_variable("DB_ENGINE"),
+        'NAME': get_env_variable("DB_NAME"),
+        'USER': get_env_variable("DB_USER"),
+        'PASSWORD': get_env_variable("DB_PASSWORD"),
+        'HOST': get_env_variable("DB_HOST"),
+        'PORT': get_env_variable("DB_PORT"),
+        'OPTIONS': {
+            'charset': 'utf8mb4',
+        }
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
