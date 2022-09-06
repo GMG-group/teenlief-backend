fe6661eb37e0bd463913c7c695986c5c3f08e145
euichan2 <euichan1108@gmail.com>
2022-09-07 00:15:39 +0900

    marker simple api

--- api/urls.py
+++ api/urls.py
@@ -5,6 +5,7 @@ from api import views
 router = DefaultRouter()
 router.register('marker', views.MarkerViewSet)
 router.register('promise', views.PromiseViewSet)
+router.register('marker-simple', views.MarkerSimpleViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
