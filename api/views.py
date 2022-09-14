a2058a8d593cef6bd2000cd428534dc9e4503497
euichan2 <euichan1108@gmail.com>
2022-09-15 03:59:12 +0900

    shelter

--- api/views.py
+++ api/views.py
@@ -1,9 +1,10 @@
-from rest_framework import viewsets, status
+from rest_framework import viewsets, status, generics
 from rest_framework.response import Response
 
 from accounts.models import User
-from api.models import Marker, Promise, Tag
-from api.serializers import MarkerSerializer, PromiseSerializer, MarkerSimpleSerializer, TagSerializer
+from api.models import Marker, Promise, Tag, Shelter
+from api.serializers import MarkerSerializer, PromiseSerializer, MarkerSimpleSerializer, TagSerializer, \
+    ShelterSerializer
 
 
 class MarkerViewSet(viewsets.ModelViewSet):
@@ -27,3 +28,8 @@ class MarkerSimpleViewSet(viewsets.ModelViewSet):
 class TagViewSet(viewsets.ModelViewSet):
     queryset = Tag.objects.all()
     serializer_class = TagSerializer
+
+
+class ShelterViewSet(viewsets.ModelViewSet):
+    queryset = Shelter.objects.all()
+    serializer_class = ShelterSerializer
