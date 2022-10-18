aa55c0527fc741f3f927b4f0c9378353cabaf2a5
PRESSO <caff1nepill@gmail.com>
2022-10-19 01:45:34 +0900

    migrate

--- api/admin.py
+++ api/admin.py
@@ -1,8 +1,4 @@
 from django.contrib import admin
 from .models import Review
 
-<<<<<<< HEAD
-=======
-# Register your models here.
->>>>>>> 8c700e821eb5547e6de524cec4ada3841dfa581c
 admin.site.register(Review)
