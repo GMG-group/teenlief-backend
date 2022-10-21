4585cdf73e2ffc1000b61ef4ba64babc6b3c8237
euichan2 <euichan1108@gmail.com>
2022-10-21 17:21:42 +0900

    remove MyMarkerSerializer

--- api/views.py
+++ api/views.py
@@ -5,7 +5,7 @@ from rest_framework.response import Response
 from accounts.models import User
 from api.models import Marker, Promise, Tag, Shelter
 from api.serializers import MarkerSerializer, PromiseSerializer, MarkerSimpleSerializer, TagSerializer, \
-    ShelterSerializer, MyMarkerSerializer
+    ShelterSerializer
 
 
 class MarkerViewSet(viewsets.ModelViewSet):
